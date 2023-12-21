27902bebb96facdd68eabdd0af82691eb5a2136d
Furki <152931069+deerborg@users.noreply.github.com>
2023-12-21 12:16:37 +0300

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,7 @@
+const dropDown = document.querySelector(".click");
+
+      dropDown.addEventListener("click", () => {
+      const drpElm = document.querySelector(".toggle");
+      drpElm.classList.toggle("dropdown");
+  })
+
